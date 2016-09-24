702430c00bc1e00037484d6d7b5e2e042cf0a8e4
Shashank <_shashank@outlook.in>
2016-09-24 17:31:03 +0530

    Mobile Responsive

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-
 window.onload = function onLoad() {
     var school_name = "Boston School";
     var school_description = "";
     var about_school_text = "The mission of Ross Montessori School is to prepare compassionate life-long learners to have a positive impact on the world.We envision a time when Ross Montessori School will be an exemplary educational institution and recognized as such all over the world. Educators from all backgrounds will observe and learn from our model, grounded in the Montessori Method. Parents will choose our school over any other with a steadfast belief in the value we create for the children, families and communities we serve.";
-    document.getElementById("about-school-text").innerHTML = about_school_text.substring(0,450)+" ...";
+    document.getElementById("about-school-text").innerHTML = about_school_text.substring(0,400)+" ...";
     document.getElementById("cover-")
 };
