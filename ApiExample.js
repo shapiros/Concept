9b1a4a9d7ca5391aca44a4cef615bda011a1d142
PC Drew <pc@thedrews.net>
2016-09-30 10:56:58 -0600

    Modifications to use the AJAX-loaded data throughout the page.

--- ApiExample.js
+++ ApiExample.js
@@ -106,7 +106,7 @@ SchoolBlocksApp.Api = function (baseUrl) {
                     "type": "video",
                     "title": "SchoolBlocks Intro",
                     "url": "https://youtu.be/xdl0UWk-1FQ",
-                    "embedUrl": "https://youtu.be/xdl0UWk-1FQ?modestbranding=1&showinfo=0&rel=0"
+                    "embedUrl": "http://www.youtube.com/embed/xdl0UWk-1FQ?modestbranding=1&amp;showinfo=0&amp;rel=0"
                 },
                 {
                     "id": 3,
